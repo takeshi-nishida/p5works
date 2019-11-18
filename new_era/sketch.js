9f9d5a31ce5c6630569bd79166c7b9804d8bf79c
takeshi-nishida <takeshi-nishida@users.noreply.github.com>
2019-11-18 23:21:24 +0900

    Update new_era

--- new_era/sketch.js
+++ new_era/sketch.js
@@ -1,9 +1,9 @@
-let font;
-let points, poly;
-let bounds;
-let balls;
+const words = [" 令和 ", " 元年 ", " 欅坂 ", " ４６ ", " 紅白 ", " ◢ "];
 
-const word = "欅坂"
+let font;
+let points, bounds, poly;
+let balls;
+let w = 0;
 
 function preload(){
  font = loadFont("NotoSansCJKjp-Regular.otf");
@@ -11,38 +11,38 @@ function preload(){
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  points = font.textToPoints(word, 0, 0, 64, {
-    sampleFactor: 1,
-    simplifyThreshold: 0
-  });
-  bounds = font.textBounds(word, 0, 0, 64);
-  poly = points.map(p => translatePoint(p));
+
+  points = [];
+  bounds = [];
+  poly = [];
+  for(let i = 0; i < words.length; i++){
+    points.push(font.textToPoints(words[i], 0, 0, 64, {
+      sampleFactor: 2,
+      simplifyThreshold: 0
+    }));
+    bounds.push(font.textBounds(words[i], 0, 0, 64));
+    poly.push(points[i].map(p => translatePoint(p, bounds[i])));
+  }
+
   balls = [];
   noStroke();
   colorMode(HSB);
-
-  // for(let i = 0; i < 1000; i++){
-  //   const x = random(0, width);
-  //   const y = random(0, height);
-  //   const angle = random(0, TWO_PI);
-  //   const vx = cos(angle) * 10;
-  //   const vy = sin(angle) * 10;
-  //   const size = random(10, 20);
-  //   balls.push(randomColorBall(x, y, vx, vy, size));
-  // }
-
 }
 
 function draw(){
   background(0);
 
-  // drawPoly();
+  if(keyIsPressed){
+    drawPoly(poly[w]);
+    return;
+  }
 
   for(let i = 0; i < balls.length; i++){
     const b = balls[i];
-    const hit = collidePointPoly(b.x, b.y, poly);
+    const hit = collidePointPoly(b.x, b.y, poly[w]);
     const dcolor = abs(hue(b.c) - 114) / 360;
-    const f = hit ? dcolor * 0.1 + 0.01 : 1;
+    const d = w % 3 == 2 ? (1 - dcolor) : dcolor;
+    const f = hit ? d * 0.08 + 0.01: 1;
     animateBall(b, f);
     drawBall(b);
   }
@@ -52,7 +52,7 @@ function draw(){
   const n = 3;
   for(let i = 0; i < n; i++){
     const x = width * (i + 1) / (n + 1);
-    const y = sin(millis() % 1000) * height + height;
+    const y = map(sin(millis() % 1000), -1, 1, 0, 1) * height;
     const angle = random(0, TWO_PI);
     const vx = cos(angle) * 10;
     const vy = sin(angle) * 10;
@@ -83,17 +83,18 @@ function drawBall(b){
   ellipse(b.x, b.y, b.r);
 }
 
-function drawPoly(){
+function drawPoly(poly){
   beginShape();
   for(let i = 0; i < poly.length; i++){
     const p = poly[i];
     vertex(p.x, p.y);
   }
-  endShape();
+  endShape(CLOSE);
 }
 
 function randomColorBall(x, y, vx, vy, r){
-  const c = color(random(360), 55, 73, 0.5);
+  const hue = randomGaussian(114, 100);
+  const c = color(constrain(hue, 0, 360), 55, 73, 0.8);
   return { x, y, vx, vy, r, c };
 }
 
@@ -103,10 +104,14 @@ function mouseDragged(){
   balls.push(randomColorBall(mouseX, mouseY, vx, vy, 10));
 }
 
-function translatePoint(p){
+function translatePoint(p, bounds){
   const x = (p.x - bounds.x) * width / bounds.w;
   const y = (p.y - bounds.y) * height / bounds.h;
   return createVector(x, y);
+}
+
+function mouseClicked(){
+  w = (w + 1) % words.length;
 }
 
 function windowResized(){
